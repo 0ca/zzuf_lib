4437176582b5367792d991cdfd1a5f4477044708
Francisco Oca <xyzthor@gmail.com>
2017-07-18 13:35:30 -0700

    Update libfuzz_test.py

--- libfuzz_test.py
+++ libfuzz_test.py
@@ -1,7 +1,7 @@
 import libzzuf
 input = "a" * 200
 print "Input: %s" % input
-seed = 0 # A value from 0 to MAX_INT
+seed = 0 # A value from -0x7FFFFFE to 0x7FFFFFFF
 ratio = 0.01 # A value from 0 to 1, read zzuf documentation
 output = libzzuf.fuzz_buffer(seed, ratio, input, len(input))
 print "Output: %s" % output
